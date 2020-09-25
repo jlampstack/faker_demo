e430e36c9fc8bc1b56bd2b0fa0c44286ae33eb53
Jay Lampstack <jlampstack@gmail.com>
2020-09-25 08:04:55 -0400

    Initialized repository

---
+++ README.md
@@ -0,0 +1,10 @@
+# STEPS
+
+- STEP 1: Insert CDN <link> to use faker
+- STEP 2: Insert &lt;script src="script.js"&gt; and create external file
+- STEP 3: Add event listener for button
+- STEP 4: Store faker.helpers.createCard(); to info variable to be able to access API methods
+- STEP 5: Use destructuring to store faker data into a variable that should match faker API varnames
+- STEP 6: Use the destructured variables to grab data and render using the DOM
+  STP
+- STEP 7: Read [faker documentation](https://www.npmjs.com/package/faker) for more features 👍
---
+++ script.js
@@ -0,0 +1,26 @@
+const btnGen = document.getElementById('generateBtn');
+const user = document.getElementsByTagName('user');
+
+btnGen.addEventListener('click', () => {
+	// stores faker API
+	const info = faker.helpers.createCard();
+
+	// destructure
+	const {
+		name,
+		email,
+		address: { city, country, zipcode },
+	} = info;
+
+	console.log(
+		faker.fake('{{name.lastName}}, {{name.firstName}} {{name.suffix}}')
+	);
+
+	// console.log(info);
+
+	document.querySelector('#name').value = name;
+	document.querySelector('#email').value = email;
+	document.querySelector('#city').value = city;
+	document.querySelector('#zipcode').value = zipcode;
+	document.querySelector('#country').value = country;
+});
